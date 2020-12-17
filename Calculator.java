a4e5f7fcf84fe4963b004e7d13333d9de6335d7f
Eshwar Krishnan S <eshwarkrishnan99@gmail.com>
2020-12-17 13:43:27 +0530

    second-commit

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,12 @@ class Calculator {
     return num1+num2;
   }
   
+  public static void int sub(int num1,int num2) {
+    return num1-num2;
+  }
+  
   public static void main(String[] args) {
     System.out.println(add(30,43));
+    System.out.println(sub(43,30));
   }
 }
