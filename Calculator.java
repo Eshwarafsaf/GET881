1cac5e9922d6cf5461b170a6160ffa1a08b8c6dd
Eshwar Krishnan S <eshwarkrishnan99@gmail.com>
2020-12-17 13:40:23 +0530

    Create Calculator.java
    first-commit

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+class Calculator {
+  public static void int add(int num1,int num2) {
+    return num1+num2;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(add(30,43));
+  }
+}
