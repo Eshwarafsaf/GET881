9eb47fdf5af378894fcae1650f11a0c0c9e04d44
Eshwar Krishnan S <eshwarkrishnan99@gmail.com>
2020-12-18 13:06:00 +0530

    Product added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,4 +8,7 @@ public class Calc {
 	public void sub(int num1, int num2) {
 		System.out.println("Difference=" + (num1 - num2));
 	}
+	public void mult(int num1, int num2) {
+		System.out.println("Product=" + (num1 * num2));
+	}
 }
