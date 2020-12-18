0df248f7c2542fecc38eda26bd883a5bfc9a0671
Eshwar Krishnan S <eshwarkrishnan99@gmail.com>
2020-12-18 13:10:33 +0530

    Added Quotient and Remainder

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -11,4 +11,7 @@ public class Calc {
 	public void mult(int num1, int num2) {
 		System.out.println("Product=" + (num1 * num2));
 	}
+	public void div(int num2, int num2) {
+		System.out.println("Quotient=" + (num1 / num2) + "\tRemainder=" + (num1 % num2));
+	}
 }
