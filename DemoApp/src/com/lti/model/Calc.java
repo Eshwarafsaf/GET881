f15225fb9fde41aa9cf13af18f7fabe81d4beedc
Eshwarafsaf <eshwarkrishnan99@gmail.com>
2020-12-18 15:51:12 +0530

    Only Add

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,10 +5,4 @@ public class Calc {
 	public void add(int num1, int num2) {
 		System.out.println("Sum=" + (num1 + num2));
 	}
-	public void sub(int num1, int num2) {
-		System.out.println("Difference=" + (num1 - num2));
-	}
-	public void mult(int num1, int num2) {
-		System.out.println("Product=" + (num1 * num2));
-	}
 }
