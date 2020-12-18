95ed507a18ced255dac0a30572f24b2c5667c1a6
Eshwarafsaf <eshwarkrishnan99@gmail.com>
2020-12-18 15:47:59 +0530

    All Methods

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,4 +8,16 @@ public class Calc {
 	public void sub(int num1, int num2) {
 		System.out.println("Difference=" + (num1 - num2));
 	}
+	public void mult(int num1, int num2) {
+		System.out.println("Product=" + (num1 * num2));
+	}
+	public void div(int num1, int num2) {
+		System.out.println("Quotient=" + (num1 / num2) + "\tRemainder=" + (num1 % num2));
+	}
+	public void si(int p,int r,int t) {
+		System.out.println("SI="+(p * r * t/100));
+	}
+	public void sqr(int p) {
+		System.out.println("Square="+(p * p));
+	}
 }
